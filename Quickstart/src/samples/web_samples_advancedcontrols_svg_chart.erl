d8aa31c05a201a4e5b3b2272378a3d550de71959
Hans Ulrich Niedermann <hun@n-dimensional.de>
2009-05-10 19:14:12 +0200

    SC: Add example line plot as standalone URL

--- Quickstart/src/samples/web_samples_advancedcontrols_svg_chart.erl
+++ Quickstart/src/samples/web_samples_advancedcontrols_svg_chart.erl
@@ -23,35 +23,7 @@ right() -> linecount:render().
 body() ->
     [
      #h2 { text="Line Chart" },
-     #svg_chart
-     {title="Line Chart",
-      width=600, height=300,
-      description="Adapted from Google Charts Example",
-      id="svg-chart__line-chart",
-      axes=
-      [#chart_axis
-       {position=bottom,
-	labels=["one", "two", "three", "four"]
-       },
-       #chart_axis
-       {position=left,
-	labels=["a", "b", "c", "d"]
-       }
-      ],
-      data=
-      [#chart_data
-       {legend="Data 1",
-	color="#ff9900",
-	line_width=3,
-	values=[10, 20, 30, 20, 30, 40, 40, 50, 60]
-       },
-       #chart_data
-       {legend="Data 2",
-	color="#2768A9",
-	values=[20, 50, 70, 90, 70, 40, 10,  1,  8]
-       }
-      ]
-     },
+     web_samples_contenttype_svg_chart:line_chart(),
      #h2 { text="Test Chart" },
      #svg_chart{title=test}
     ].
---
+++ Quickstart/src/samples/web_samples_contenttype_svg_chart.erl
@@ -0,0 +1,41 @@
+-module (web_samples_contenttype_svg_chart).
+-include ("wf.inc").
+-include ("svg_chart.inc").
+-compile(export_all).
+
+main() ->
+	wf:set_content_type("image/svg+xml"),
+	line_chart().
+
+event(_) -> ok.
+
+line_chart() ->
+    #svg_chart
+	    {title="Line Chart",
+	     width=600, height=300,
+	     description="Adapted from Google Charts Example",
+	     id="svg-chart__line-chart",
+	     axes=
+	     [#chart_axis
+	      {position=bottom,
+	       labels=["one", "two", "three", "four"]
+	      },
+	      #chart_axis
+	      {position=left,
+	       labels=["a", "b", "c", "d"]
+	      }
+	     ],
+	     data=
+	     [#chart_data
+	      {legend="Data 1",
+	       color="#ff9900",
+	       line_width=3,
+	       values=[10, 20, 30, 20, 30, 40, 40, 50, 60]
+	      },
+	      #chart_data
+	      {legend="Data 2",
+	       color="#2768A9",
+	       values=[20, 50, 70, 90, 70, 40, 10,  1,  8]
+	      }
+	     ]
+	    }.
